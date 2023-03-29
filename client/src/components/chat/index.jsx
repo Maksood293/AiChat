9c2425656af8c602a84c0d8b8b469a752b010450
Maksood293 <maksoodkhan0143@gmail.com>
2023-03-29 22:28:28 +0530

    Created AiChat

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+import CustomHeader from "../customHeader";
+import Ai from "../customMessageForm/Ai";
+import AiCode from "../customMessageForm/AiCode";
+import StandardMessageForm from "../customMessageForm/StandardMessageForm";
+
+const Chat = () => {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "testuser",
+    "1234"
+  );
+  return (
+    <div style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <CustomHeader chat={chat} />}
+        renderMessageForm={(props) => {
+          if (chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiCode_")) {
+            return <AiCode props={props} activeChat={chatProps.chat} />;
+          }
+          return (
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
+        }}
+      />
+    </div>
+  );
+};
+
+export default Chat;
